c8c66e2acde109302f79f87a63018ed594427563
Eric Li <aquaird@yahoo.com>
2018-06-26 23:42:31 +0800

    ignore & bugs in LSTM fixed

--- src/models/LSTM.py
+++ src/models/LSTM.py
@@ -10,7 +10,8 @@ class GraphLSTM(nn.Module):
         self.chains = [[15,14,13], [10,11,12], [5,4,3], [0,1,2]]
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.cell = nn.LSTMCell(self.input_size, self.hidden_size)
+        self.fcell = nn.LSTMCell(self.input_size, self.hidden_size)
+        self.bcell = nn.LSTMCell(self.input_size, self.hidden_size)
         self.refine_layer = nn.Linear(self.hidden_size, self.input_size)
 
     def forward(self, p3d):
@@ -23,29 +24,29 @@ class GraphLSTM(nn.Module):
         forward_h = Variable(torch.zeros(pre_p3d.size(0), 16, self.hidden_size)).cuda()
         forward_c = Variable(torch.zeros(pre_p3d.size(0), 16, self.hidden_size)).cuda()
         for chain in self.chains:
-            forward_h[:,chain[0],:], forward_c[:,chain[0],:] = self.cell(pre_p3d[:,chain[0],:])
-            forward_h[:,chain[1],:], forward_c[:,chain[1],:] = self.cell(pre_p3d[:,chain[1],:], (forward_h[:,chain[0],:], forward_c[:,chain[0],:]))
-            forward_h[:,chain[2],:], forward_c[:,chain[2],:] = self.cell(pre_p3d[:,chain[2],:], (forward_h[:,chain[1],:], forward_c[:,chain[1],:]))
+            forward_h[:,chain[0],:], forward_c[:,chain[0],:] = self.fcell(pre_p3d[:,chain[0],:])
+            forward_h[:,chain[1],:], forward_c[:,chain[1],:] = self.fcell(pre_p3d[:,chain[1],:], (forward_h[:,chain[0],:], forward_c[:,chain[0],:]))
+            forward_h[:,chain[2],:], forward_c[:,chain[2],:] = self.fcell(pre_p3d[:,chain[2],:], (forward_h[:,chain[1],:], forward_c[:,chain[1],:]))
 
-        hx = torch.div(torch.add(forward_h[:,2,:], forward_h[:,3,:]))
-        cx = torch.div(torch.add(forward_c[:,2,:], forward_c[:,3,:]))
-        forward_h[:,6,:], forward_c[:,6,:] = self.cell(pre_p3d[:,6,:], (hx, cx))
-        forward_h[:,7,:], forward_c[:,7,:] = self.cell(pre_p3d[:,7,:], (forward_h[:,6,:], forward_c[:,6,:]))
+        hx = (forward_h[:,2,:]+forward_h[:,3,:])/2
+        cx = (forward_c[:,2,:]+forward_c[:,3,:])/2
+        forward_h[:,6,:], forward_c[:,6,:] = self.fcell(pre_p3d[:,6,:], (hx, cx))
+        forward_h[:,7,:], forward_c[:,7,:] = self.fcell(pre_p3d[:,7,:], (forward_h[:,6,:], forward_c[:,6,:]))
 
         hx = (forward_h[:,7,:] + forward_h[:,13,:] + forward_h[:,12,:])/3
         cx = (forward_c[:,7,:] + forward_c[:,13,:] + forward_c[:,12,:])/3
-        forward_h[:,8,:], forward_c[:,8,:] = self.cell(pre_p3d[:,8,:], (hx, cx))
-        forward_h[:,9,:], forward_c[:,9,:] = self.cell(pre_p3d[:,9,:], (forward_h[:,8,:], forward_c[:,8,:]))
+        forward_h[:,8,:], forward_c[:,8,:] = self.fcell(pre_p3d[:,8,:], (hx, cx))
+        forward_h[:,9,:], forward_c[:,9,:] = self.fcell(pre_p3d[:,9,:], (forward_h[:,8,:], forward_c[:,8,:]))
 
         # backward LSTM
         back_h = Variable(torch.zeros(pre_p3d.size(0), 16, self.hidden_size)).cuda()
         back_c = Variable(torch.zeros(pre_p3d.size(0), 16, self.hidden_size)).cuda()
 
-        back_h[:,9,:], back_c[:,9,:] = self.cell(pre_p3d[:,9,:])
+        back_h[:,9,:], back_c[:,9,:] = self.bcell(pre_p3d[:,9,:])
         hx = back_h[:,9,:]
         cx = back_c[:,9,:]
         for i in [8,7,6]:
-            back_h[:,i,:], back_c[:,i,:] = self.cell(pre_p3d[:,i,:], (hx,cx))
+            back_h[:,i,:], back_c[:,i,:] = self.bcell(pre_p3d[:,i,:], (hx,cx))
             hx = back_h[:,i,:]
             cx = back_c[:,i,:]
         
@@ -57,7 +58,7 @@ class GraphLSTM(nn.Module):
                 hx = back_h[:,6,:]
                 cx = back_c[:,6,:]
             for i in chain.reverse():
-                hx,cx = self.cell(pre_p3d[:,i,:], (hx,cx))
+                hx,cx = self.bcell(pre_p3d[:,i,:], (hx,cx))
                 back_h[:,i,:] = hx
                 back_c[:,i,:] = cx
 
